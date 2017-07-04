00a2d6124821f342b5ff9fb7d0b85a747435831a
Zulu55 <jzuluaga55@gmail.com>
2017-07-04 10:01:07 -0500

    Initial commit

---
+++ DivisasNative/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DivisasNative
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
---
+++ Droid/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace DivisasNative.Droid
+{
+    [Activity(Label = "Divisas Native", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+		#region Controls
+		EditText editTextPesos;
+		EditText editTextDollars;
+		EditText editTextEuros;
+		EditText editTextPounds;
+        Button buttonConvert;
+		#endregion
+
+		protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+			editTextPesos = FindViewById<EditText>(Resource.Id.editTextPesos);
+			editTextDollars = FindViewById<EditText>(Resource.Id.editTextDollars);
+			editTextEuros = FindViewById<EditText>(Resource.Id.editTextEuros);
+			editTextPounds = FindViewById<EditText>(Resource.Id.editTextPounds);
+			buttonConvert = FindViewById<Button>(Resource.Id.buttonConvert);
+
+            buttonConvert.Click += ButtonConvert_Click;
+		}
+
+        void ButtonConvert_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
+
